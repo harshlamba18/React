3facb6136475bfe0955af015f84bddf8a8f53daa
harshlamba18 <harshlamba70144@gmail.com>
2025-05-24 14:52:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Dashboard from "./Dashboard";
+import EditProfile from "./EditProfile";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import ThemeToggler from "./Theme";
+
+const allRoutes = createBrowserRouter(
+  [
+    { path: "/", element: <Login /> },
+    { path: "/login", element: <Login /> },
+    { path: "/signup", element: <SignUp /> },
+    { path: "/dashboard", element: <Dashboard /> },
+    { path: "/editprofile", element: <EditProfile /> },
+  ],
+);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={allRoutes} />
+      <ToastContainer position="top-center" autoClose={2000} />
+      <ThemeToggler />
+    </>
+  );
+}
+
+export default App;
